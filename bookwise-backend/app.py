eb71cea2761ba51af51687bc5609ff90dc4773f5
altasbatuhan <altasbatuhan1@gmail.com>
2024-11-22 01:24:37 +0300

    Update app.py to disable debug mode and modify docker-compose for versioned images

--- bookwise-backend/app.py
+++ bookwise-backend/app.py
@@ -735,5 +735,6 @@ def get_all_categories():
     except Exception as e:
         print("An error occurred while fetching categories:", e)
         return jsonify({"error": "An error occurred", "message": str(e)}), 500
+    
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5005)
+    app.run(debug=False, host="0.0.0.0", port=5005)
